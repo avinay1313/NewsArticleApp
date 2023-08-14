eb94407213887e590b5dc9e70b245ac8ee844c29
Avinay kumar <akumar@rdglobalinc.com>
2023-08-14 16:55:58 +0530

    commit changes

---
+++ NewsArticleApp/APIService/APIService.swift
@@ -0,0 +1,16 @@
+//
+//  APIService.swift
+//  NewsArticleApp
+//
+//  Created by Admin on 14/08/23.
+//
+
+import Foundation
+
+class APIService {
+    
+    static let shared = APIService()
+    
+    
+    
+}
--- NewsArticleApp/ArticleListing/ArticlesListingVC.swift
+++ NewsArticleApp/ArticleListing/ArticlesListingVC.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class ArticlesListingVC: UIViewController {
+    
+    @IBOutlet weak var tblArticles: UITableView!
+    
+    let viewModel = ArticlesListViewModel()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tblArticles.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+}
+
+
+extension ArticlesListingVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = "Message"
+        return cell
     }
-    */
-
 }

756398883d2ba5727ac8f8dbe024a2ddf1811185
Avinay kumar <akumar@rdglobalinc.com>
2023-08-14 18:07:01 +0530

    commit changes

---
+++ NewsArticleApp/CDModels/CDArticle+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDArticle+CoreDataClass.swift
+//  
+//
+//  Created by Admin on 14/08/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDArticle)
+public class CDArticle: NSManagedObject {
+
+}
---
+++ NewsArticleApp/CDModels/CDArticle+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  CDArticle+CoreDataProperties.swift
+//  
+//
+//  Created by Admin on 14/08/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CDArticle {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CDArticle> {
+        return NSFetchRequest<CDArticle>(entityName: "CDArticle")
+    }
+
+    @NSManaged public var url: String?
+    @NSManaged public var title: String?
+
+}
